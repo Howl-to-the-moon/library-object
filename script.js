3239e8ca36257353e18834c5d94607dfa749bf6f
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2026-02-21 15:26:01 -0500

    ok so uh i got it so a function adds a new div to the main container with the class it needs.
    just gotta add the other divs with the content
    from its specific place in the array.

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const myLibrary = [];
 
 function Book(title, author, pageCount, isRead){
 
+    if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
@@ -18,9 +22,21 @@ function addBookToLibrary(title, author, pageCount, isRead){
    myLibrary.push(temp);
 };
 
+
+    
+
 function displayBooks(myLibrary){
-    for (i = myLibrary.length; i > 0 ; i--){
+    let display = document.querySelector(".mainContainer");
+    for (i = 0; i < myLibrary.length ; i++){
         /* make it so a new div with a class for the card template n stuff is made that cocntains the content from each book. this for statement is messsed up so research and fix when u work on this later */
+       let newCard = document.createElement("div");
+        newCard.classList.add("libraryCard");
+        let newTitle = document.createElement("div");
+
+        display.append(newCard);
+
     }
 };
+
+addBookToLibrary("Apathy", "Benson", 235, false);
 
