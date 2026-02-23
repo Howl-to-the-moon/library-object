057e5de395fdf85cb2566f48cb724ead7d812028
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2026-02-23 16:35:00 -0500

    help me

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 console.log("yooch");
 
 
@@ -12,7 +14,13 @@ function Book(title, author, pageCount, isRead){
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
-    this.isRead = isRead;
+    this.isRead = isRead; 
+  console.log(isRead);
+  if (isRead == false){
+    isRead.textContent = "unread";
+  } else {
+    isRead.textContent = "read";
+  }
     this.id = crypto.randomUUID();
 
 };
@@ -26,7 +34,7 @@ function addBookToLibrary(title, author, pageCount, isRead){
     
 
 function displayBooks(myLibrary){
-    let display = document.querySelector(".mainContainer");
+    let display = document.querySelector(".bookContainer");
     for (i = 0; i < myLibrary.length ; i++){
         /* make it so a new div with a class for the card template n stuff is made that cocntains the content from each book. this for statement is messsed up so research and fix when u work on this later */
        let newCard = document.createElement("div");
@@ -41,14 +49,75 @@ function displayBooks(myLibrary){
         let pageCount = document.createElement("div");
         pageCount.textContent = myLibrary[i].pageCount;
 
+        let holder = document.createElement("div");
+        holder.classList.add("grid");
+
         let isRead = document.createElement("div");
         isRead.textContent = myLibrary[i].isRead;
+
+        let toggle = document.createElement("button");
+        toggle.textContent = "Read?";
         
-        newCard.append(newTitle, newAuth, pageCount, isRead);
+        holder.append(isRead, toggle);
+        newCard.append(newTitle, newAuth, pageCount, holder);
         display.append(newCard);
 
     }
 };
 
+let popUp = document.querySelector(".form");
+function showPopUp(){
+    popUp.classList.remove("hide");
+    popUp.classList.add("show");
+};
+
+function hidePopUp(){
+    popUp.classList.remove("show");
+    popUp.classList.add("hide");
+};
+
+let addBook = document.querySelector(".addBook");
+let cancel = document.querySelector(".cancel");
+let submit = document.querySelector(".submit")
+
+ addBook.addEventListener("click", () => {
+    showPopUp();
+}); 
+
+cancel.addEventListener("click", () => {
+    hidePopUp();
+});
+
+submit.addEventListener("click", () => {
+    
+    let a = document.getElementById('book_title');
+    let b = document.getElementById('book_author');
+    let c = document.getElementById('pageCount');
+    let d = document.getElementById('isRead');
+    addBookToLibrary(a.value, b.value, c.value, d.value);
+
+     clearAll(); 
+    displayBooks(myLibrary);
+    hidePopUp();
+});
+
+ function clearAll(){
+    let main = document.querySelector(".bookContainer");
+    while (main.firstChild){
+        main.removeChild(main.lastChild);
+    }
+
+    let a = document.getElementById('book_title');
+    let b = document.getElementById('book_author');
+    let c = document.getElementById('pageCount');
+    let d = document.getElementById('isRead');
+
+    a.value = '';
+    b.value = '';
+    c.value = '';
+    d.value = '';
+};
+ 
 addBookToLibrary("Apathy", "Benson", 235, false);
+
 displayBooks(myLibrary);
